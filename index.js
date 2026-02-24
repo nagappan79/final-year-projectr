f9cf50329e2340e0332d398b34a01abd7c3a49b0
nagappan79 <nagappanpanimalar@gmail.com>
2026-02-25 00:43:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
+// Hardcoded secret (detectable)
+const API_KEY = "123456789SECRETKEY";
+
 app.get('/', (req, res) => {
-  eval(req.query.code);  // Vulnerable
+  res.send("Hello World");
 });
 
 app.listen(3000);
